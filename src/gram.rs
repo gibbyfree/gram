20c19ecf7e6d0c32fba470d61c6d887af8922c9f
Gibby Free <free.g@husky.neu.edu>
2022-09-21 16:12:18 -0700

    refactor

--- src/gram.rs
+++ src/gram.rs
@@ -1,19 +1,32 @@
 use termion::raw::IntoRawMode;
 use std::io::{self, Read, stdout};
 
-pub fn main() {
-    let mut c: char;
-    let _stdout = stdout().into_raw_mode().unwrap();
+fn is_quit_byte(b: &u8) -> bool {
+    *b == 17 as u8
+}
+
+fn editor_read_key() -> u8 {
     for b in io::stdin().bytes() {
         let b = b.unwrap();
-        c = b as char;
-        if c.is_control() {            
-            println!("{:?} \r", b);            
-        } else {            
-            println!("{:?} ({})\r", b, c);            
-        }
-        if c == 'q' {
-            break;
-        }
+        return b;
+    }
+    0
+}
+
+fn editor_proc_key() -> u8 {
+    let b = editor_read_key();
+
+    match b {
+        b if is_quit_byte(&b) => return 0,
+        _ => return 1,
+    };
+}
+
+pub fn main() {
+    let _stdout = stdout().into_raw_mode().unwrap();
+    let mut exit = 1;
+
+    while exit != 0 {
+        exit = editor_proc_key();
     }
 }
